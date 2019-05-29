6c1bf4c0632ef5308754d409e227f56beb64be68
Xiaofeng Wei <xiaofeng.wei@nxp.com>
2019-05-29 08:02:13 +0000

    x

--- compile.sh
+++ compile.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 echo "Compiling"
+
+cd face_recognition/
+make
+cd image_classification/
+make
+cd object_detection/
+make
+
+cp face_recognition/FaceRecognition.so webserver/inference/
+cp image_classification/ImageClassification.so webserver/inference/
+cp object_detection/ObjectDetection.so webserver/inference/
+
+cp -a webserver target
